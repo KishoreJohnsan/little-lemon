3c934cddd6f13728d4d91013887d24819b493f47
Kishore Johnsan <kishore.johnsan@ticketmaster.com>
2025-07-01 19:06:02 +0530

    :wrench: update config

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -17,4 +17,12 @@ export default defineConfig({
       ],
     },
   },
+   html: {
+    template: './public/index.html',
+    title: 'Little Lemon | Authentic Mediterranean Dining',
+    favicon: './src/assets/littlelemon.svg',
+    meta: {
+      description: 'Discover the rich flavors of the Mediterranean at our family-owned restaurant. Enjoy traditional recipes with a modern twist, fresh daily specials, and warm hospitality in a cozy, elegant setting.',
+    },
+  },
 });
